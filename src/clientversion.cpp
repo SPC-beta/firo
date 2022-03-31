f8e9890dda6f4e49ace849460916f129098c83d3
bzxtests11 <bzxtests11@protonmail.com>
2022-03-31 23:47:39 +0300

    z

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -45,8 +45,8 @@ const std::string CLIENT_NAME("Satoshi");
 //! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "904984eecd"
-#define GIT_COMMIT_DATE "Fri, 18 Mar 2022 08:24:52 +0800"
+#define GIT_COMMIT_ID "78e29d68b7"
+#define GIT_COMMIT_DATE "Wed, 9 Mar 2022 08:51:36 +0800"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
